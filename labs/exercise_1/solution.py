1703992e49c015f80275bd02e61d72ea125e14f4
ficodegallo <nbfico@gmail.com>
2015-02-19 21:42:59 -0500

    Quick update
    Moved the randomized list out of the function to make the function
    itself reusable for any list

--- labs/exercise_1/solution.py
+++ labs/exercise_1/solution.py
@@ -1,10 +1,11 @@
 import random
 
-def divide_33():
-    random_numbers = random.sample(xrange(10000), 100)
+numbers_list = random.sample(xrange(10000), 100)
 
-    for num in random_numbers:
+def divide_33(numbers):
+
+    for num in numbers:
 	    if num % 33 == 0:
 		    print num
 
-divide_33() 
+divide_33(numbers_list) 
