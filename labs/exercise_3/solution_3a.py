b3a3b807de3811840e50095d5ad525e569dc047b
ficodegallo <nbfico@gmail.com>
2015-02-21 16:48:20 -0500

    Exercise 3 1st Question
    Answer to the 1st question in exercise 3

---
+++ labs/exercise_3/solution_3a.py
@@ -0,0 +1,26 @@
+#import the csv library
+import csv
+
+#read in the rock.csv file and save as rock_data
+with open('rock.csv', 'rb') as rock_data:
+#store rock_data into reader
+    reader = csv.reader(rock_data)
+
+#count the amount of songs released in 1981
+    count = 0
+    for row in reader:
+    	if row[2] =='1981':
+    		count += 1
+
+    print count
+
+rock_data.close()
+
+""""
+    #read the header of the file
+    header = reader.next()
+    print header
+"""
+
+    
+  
